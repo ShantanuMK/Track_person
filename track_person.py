e87115b7048d96f2db071e2b07975de673f098e8
ShantanuMK <59115222+ShantanuMK@users.noreply.github.com>
2021-05-31 15:39:53 +0530

    Update track_person.py

--- track_person.py
+++ track_person.py
@@ -4,22 +4,22 @@ import time
 tts = pyttsx3.init()
 
 #replace ip address with the allocated ip for device to be tracked
-my_ip= "192.168.**.**"
+ip= "192.168.**.**"
 
 c = 0
 
 while True: 
-    pingip = subprocess.Popen(['ping' , my_ip] , stdout=subprocess.PIPE)
+    pingip = subprocess.Popen(['ping' , ip] , stdout=subprocess.PIPE)
     time.sleep(4)
     if(pingip.poll() == 0 ):
         if c == 0:
-            tts.say(x + " just connected to the internet!")
+            tts.say("Shantanu just connected to the internet!")
             tts.runAndWait()
         c = 1
         print("user is connected")
     else:
         if c == 1:
-            tts.say(x + " just disconnected from the internet!")
+            tts.say("Shantanu just disconnected from the internet!")
             tts.runAndWait()
         c = 0
         print("user is disconnected")
